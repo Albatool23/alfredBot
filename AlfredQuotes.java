dd5c45ec3873ec9869f2d98adefa3fc7c6550a7b
albatool <a23@test.com>
2022-07-28 14:43:37 +0300

    complete

--- AlfredQuotes.java
+++ AlfredQuotes.java
@@ -1,36 +1,35 @@
 import java.util.Date;
 public class AlfredQuotes {
-    
+
     public String basicGreeting() {
         // You do not need to code here, this is an example method
         return "Hello, lovely to see you. How are you?";
     }
-    
+
     public String guestGreeting(String name ) {
-        return String.format("Good %s,. Lovely to see you." , name);
+        
+        return String.format("Hello ,. Lovely to see you." , name);
     }
-    
+
 
 
     public String dateAnnouncement() {
-        return String.format("It is currently %s ", new Date());
+        Date date = new Date();
+        return "Current date is " + date;
     }
-    
 
 
     public String respondBeforeAlexis(String conversation) {
-     
-        if(conversation.indexOf("Alexis") > -1) {
-            return "She's really of no help. What can I get for you?";
-        }
 
-        if (conversation.indexOf("Alfred") > -1) {
-            return "At your service, naturally. How may I be of assistance?";
+        if (conversation.contains("Alexis")){
+            return conversation + "Right away, sir. She certainly isn't sophisticated enough for that.";
+        }else if(conversation.contains("Alfred")){
+            return conversation + "At your service. As you wish, naturally.";
+        }else{
+            return conversation + "Right. And with that I shall retire.";
         }
+    }
 
-        return "Right. And with that I shall retire.";
-    }
-    }
- 
+}
 
 
